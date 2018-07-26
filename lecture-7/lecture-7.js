67f19a0ea3dc81bcab5820cfb2cc1cc5dfbb0366
shradha <shradhakhapra1899@gmail.com>
2018-07-26 10:28:33 +0530

    lecture-7 added

---
+++ lecture-7/lecture-7.js
@@ -0,0 +1,8 @@
+(function(){
+    var a;
+    document.getElementById('button').onclick=function(){
+        a= document.getElementById('inp').value;
+        document.getElementById('result').innerHTML=a;
+    }
+
+})();
